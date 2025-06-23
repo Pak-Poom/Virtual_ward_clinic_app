f8698d8bab938b716399b51382eeeb9d236e18ea
Pak-PoOm <118786397+Pak-Poom@users.noreply.github.com>
2025-06-23 13:33:46 +0700

    Move the teimporary displaying the messeage out of the st.form() block

--- virtual_ward_clinic_app.py
+++ virtual_ward_clinic_app.py
@@ -107,14 +107,14 @@ with st.form(key="data_form", clear_on_submit=True):
         else:
             st.error("กรุณากรอกข้อมูลให้ครบถ้วนและถูกต้อง!!!")
 
-    # Show message if submission was successful
-    if st.session_state.get("submission_success", False):
-        st.success("ข้อมูลได้ถูกบันทึกเรียบร้อยแล้ว!")
-    
-        # If more than 3 seconds have passed since the message was set, clear it
-        if time.time() - st.session_state.get("timestamp", 0) > 2:
-            st.session_state["submission_success"] = False
-            st.rerun()
+# Show message if submission was successful
+if st.session_state.get("submission_success", False):
+    st.success("ข้อมูลได้ถูกบันทึกเรียบร้อยแล้ว!")
+
+    # If more than 3 seconds have passed since the message was set, clear it
+    if time.time() - st.session_state.get("timestamp", 0) > 2:
+        st.session_state["submission_success"] = False
+        st.rerun()
 
 with st.sidebar:
     # Display data in the sidebar view
