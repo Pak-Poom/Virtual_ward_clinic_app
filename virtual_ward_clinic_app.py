4a135cd9b04dd4fb0e606ce909da9d6504dc1358
Pak-PoOm <118786397+Pak-Poom@users.noreply.github.com>
2025-07-14 11:41:15 +0700

    Update virtual_ward_clinic_app.py

--- virtual_ward_clinic_app.py
+++ virtual_ward_clinic_app.py
@@ -32,10 +32,9 @@ SHEET_NAME = 'Form_Records'
 sheet_by_name = connect_to_gsheet(SPREADSHEET_NAME, sheet_name=SHEET_NAME)
 
 # st.set_page_config(page_title="Virtual Ward Clinic", layout="wide")
-st.title("🏥    Virtual Ward Clinic")
+st.title("Virtual Ward Clinic ⸝⸝⸝ 🚑")
+st.write("ᯓᡣ𐭩 ศูนย์หัวใจและหลอดเลือด -โรงพยาบาลจุฬาภรณ์")
 st.write("")
-st.write("ﮩ٨ـﮩﮩ٨ـ♡ﮩ٨ـﮩﮩ٨ـ ")
-st.write(": ศูนย์หัวใจและหลอดเลือด -โรงพยาบาลจุฬาภรณ์")
 
 # Read Data from Google Sheets
 def read_data():
